06f3fb81eba1a06ffd5de91e64a5bc92afdd7844
Tomás Abril <tomasabril@fake.noreply.com>
2018-05-12 16:32:59 -0300

    exercicio debounce

--- 18_04_25/ex_1/ex_1.vhd
+++ 18_04_25/ex_1/ex_1.vhd
@@ -1,38 +1,44 @@
-library ieee;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
---
-ENTITY ex_1  IS 
-	GENERIC (N : NATURAL := 4);
-	PORT(
-		entrada : IN STD_LOGIC_VECTOR (N-1 DOWNTO 0);
-		saida : OUT STD_LOGIC_VECTOR (N-1 DOWNTO 0)
-		);
-END ENTITY;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE ieee.numeric_std.ALL;
+ENTITY
+	ex_1 IS
+	PORT (
+		clock_in : IN STD_LOGIC;
+		botao    : IN STD_LOGIC;
+		led      : OUT STD_LOGIC
+	);
+END ex_1;
 
 ARCHITECTURE ex_1 OF ex_1 IS
-	type vecOfNum is array (N-1 downto 0) of std_logic_vector(3 downto 0);
-	type parcialSum is array (N-2 downto 0) of Signed(3 downto 0);
-	SIGNAL s0 : vecOfNum;
-	SIGNAL sum : parcialSum;
-	signal total : integer;
 BEGIN
-	
-	g_GENERATE_FOR: for i in 0 to N-1 generate
-		s0(i) <= ( '0','0','0', entrada(i));
-	end generate g_GENERATE_FOR;
- 
-	sum(0) <= signed(s0(0)) + signed(s0(1));
-	
-	sum_GENERATE_FOR: for i in 1 to N-2 generate
-		sum(i) <= sum(i-1) + signed(s0(i+1));
-	end generate sum_GENERATE_FOR;
 
-	total <= to_integer(sum(N-2));
+	PROCESS (clock_in)
+		// clock é de 50_000_000 por segundo?
+		VARIABLE debounce_counter : INTEGER := 0;
+		VARIABLE led_state    : STD_LOGIC := 0;
+	BEGIN
+		IF rising_edge(clock_in) THEN
+			IF (debounce_counter > 5_000_000) THEN
+				// 0, 1 segundos
+				debounce_counter := 0;
+			END IF;
 
-	f_GENERATE_FOR: for i in 0 to N-1 generate
-		saida(N-i-1) <=  '0'  WHEN total < i or total = i ELSE '1';
-	end generate f_GENERATE_FOR;
-	
+			IF (botao = '1') THEN
+				IF (debounce_counter > 0) THEN
+					debounce_counter := (debounce_counter + 1);
+				END IF;
+				IF (debounce_counter = 0) THEN
+					debounce_counter := (debounce_counter + 1);
+					IF (led_state = '1') THEN
+						led_state := '0';
+					ELSIF (led_state = '0') THEN
+						led_state := '1';
+					END IF;
+				END IF;
+			END IF;
 
-END ARCHITECTURE;
+			led <= led_state;
+
+		END PROCESS;
+	END ARCHITECTURE;
