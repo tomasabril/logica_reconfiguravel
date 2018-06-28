b54cfb8d6e4c5c1bb09665ebb99ed7851beb2ffa
Allan P <allanpatrick18@gmail.com>
2018-06-27 21:46:56 -0300

    #teste debug

--- 18_06_06/ex_01/ex_01.vhd
+++ 18_06_06/ex_01/ex_01.vhd
@@ -1,199 +1,192 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE ieee.numeric_std.ALL;
-ENTITY ex_01 IS
-
+ENTITY
+    ex_01 IS
     GENERIC
     (
         n : INTEGER := 10
     );
-
     PORT
     (
-        --input
-        but_up      : IN STD_LOGIC;
-        but_down    : IN STD_LOGIC;
-        --process
-        clk         : IN STD_LOGIC;
-        rst         : IN STD_LOGIC;
-        --output
-        output      : OUT STD_LOGIC_VECTOR (n - 1 DOWNTO 0);
-        water_level : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)
+        clock_in    : IN STD_LOGIC;
+        reset       : IN STD_LOGIC;
+        -- interface do operador
+        btn_add     : IN STD_LOGIC;
+		  btn_rm      : IN STD_LOGIC;
+        -- para mostrar qual sinal esta aberto
+        output_led  : OUT STD_LOGIC_VECTOR (n - 1 DOWNTO 0);
+        -- displays
+        output_seg  : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
+		  output_seg_count  : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)
     );
-
 END ex_01;
+
 ARCHITECTURE ex_01 OF ex_01 IS
 
-    SIGNAL queue                     : STD_LOGIC_VECTOR(100 DOWNTO 0);
-    SIGNAL output_buffer             : STD_LOGIC_VECTOR(n - 1 DOWNTO 0);
-	 SIGNAL x                         : INTEGER;
-    SHARED VARIABLE queue_lenght_var : INTEGER;
-
-	 SIGNAL but_up_o, but_down_o: std_logic;
-	 signal deposit_sig: integer;
-	 SIGNAL saidabin    : STD_LOGIC_VECTOR(11 DOWNTO 0);
+    SIGNAL final       : INTEGER;
+    SIGNAL saidabin    : STD_LOGIC_VECTOR(11 DOWNTO 0);
     SIGNAL saidadecbin : STD_LOGIC_VECTOR(3 DOWNTO 0);
+	 
+	 SIGNAL saidabin_count  : STD_LOGIC_VECTOR(11 DOWNTO 0);
+    SIGNAL saidadecbin_count : STD_LOGIC_VECTOR(3 DOWNTO 0);
+	 
+	 SIGNAL saidabin_water   : STD_LOGIC_VECTOR(11 DOWNTO 0);
+	 SIGNAL saidadecbin_water : STD_LOGIC_VECTOR(9 DOWNTO 0);
+    -- Build an enumerated type for the state machine
+    TYPE state_type IS (INIT, SHOW, ANI);
+    -- Register to hold the current state
+    SIGNAL state  		: state_type;
+    SIGNAL water_level 	: INTEGER;
+	 SIGNAL show_count 	: INTEGER;
+	 SIGNAL vector_out   : STD_LOGIC_VECTOR(9 DOWNTO 0);
+	 SIGNAL queue        : STD_LOGIC_VECTOR(100 DOWNTO 0);
+
 
 BEGIN
-	debounce_up: entity work.debounce port map(but_up, clk, but_up_o);
-	debounce_down: entity work.debounce port map(but_down, clk, but_down_o);
 
-    output <= "0000000000" when output_buffer = "0000000000" elsE
-				 "1000000000" when output_buffer = "0000000001" elsE
-				 "1100000000" when output_buffer = "0000000010" elsE
-				 "1110000000" when output_buffer = "0000000011" elsE
-				 "1111000000" when output_buffer = "0000000100" elsE
-				 "1111100000" when output_buffer = "0000000101" elsE
-				 "1000000000" when output_buffer = "1000000000" elsE
-				 "1100000000" when output_buffer = "0100000000" elsE
-				 "1110000000" when output_buffer = "1100000000" elsE
-				 "0000000000" when output_buffer = "0000000000" else
-				"0000110000";
-
-    --process to queue up pressed buttons
-    QUEUEING : PROCESS (clk, rst) IS
-        VARIABLE but_plus               : BOOLEAN;
-        VARIABLE but_plus_still_pressed : BOOLEAN;
-        VARIABLE but_min                : BOOLEAN;
-        VARIABLE but_min_still_pressed  : BOOLEAN;
-
+    PROCESS (clock_in, reset)
+        -- button after debounce
+        VARIABLE queue_pos         : INTEGER;
+        VARIABLE counter           : INTEGER;
+		  VARIABLE counter_level     : INTEGER;
+		  VARIABLE up_down           : INTEGER;
+		  VARIABLE up_down_pos       : INTEGER;
+		  VARIABLE aux_level         : INTEGER;
+		  VARIABLE count_level       : INTEGER;
+		  VARIABLE enchimento        : INTEGER;
+		  VARIABLE vector_level      : STD_LOGIC_VECTOR(9 DOWNTO 0);
+		  
     BEGIN
-        IF (rst = '1') THEN
-            queue_lenght_var       := 0;
-            but_plus               := false;
-            but_min                := false;
-            but_plus_still_pressed := false;
-            but_min_still_pressed  := false;
-        
-        elsIF (rising_edge(clk)) THEN
-
---            IF (but_up = '0') THEN
---                -- button is currently pressed
---                IF (but_plus_still_pressed = false) THEN
---                    but_plus               := true;
---                    but_plus_still_pressed := true;
---                ELSE
---                    but_plus := false;
---                END IF;
---            ELSIF (but_up = '1') THEN
---                -- button is not currently pressed
---                --bot               := false;
---                but_plus_still_pressed := false;
---            END IF;
---
---            IF (but_down = '0') THEN
---                -- button is currently pressed
---                IF (but_min_still_pressed = false) THEN
---                    but_min               := true;
---                    but_min_still_pressed := true;
---                ELSE
---                    but_min := false;
---                END IF;
---            ELSIF (but_down = '1') THEN
---                -- button is not currently pressed
---                --bot               := false;
---                but_min_still_pressed := false;
---            END IF;
-
-            --condition to lower queue
-            IF (x = 1 AND queue_lenght_var > 0) THEN
-                queue <= STD_LOGIC_VECTOR(SHIFT_LEFT(UNSIGNED(queue), 1));
-                queue_lenght_var := queue_lenght_var - 1;
-            END IF;
-
-            --adds a 1 to the queue
-            IF (but_up_o = '0') THEN
-                queue_lenght_var := queue_lenght_var + 1;
-                queue(queue_lenght_var) <= '1';
-					 --but_plus := false;
-            END IF;
-
-            --adds a 0 to the queue
-            IF (but_down_o = '0') THEN
-                queue_lenght_var := queue_lenght_var + 1;
-                queue(queue_lenght_var) <= '0';
-					 --but_min := false;
-            END IF;
-
-            --queue_lenght <= queue_lenght_var;
-
+        IF reset = '1' THEN
+            counter := 0;
+            state <= init;
+				queue_pos := 0;
+				up_down_pos:=0;
+				enchimento:=0;
+				counter_level:=0;
+				aux_level:=0;
+				count_level:=0;
+				queue <= (others => '0');
+				vector_level:= (others => '0');
+				water_level <=0;
+			ELSIF rising_edge(clock_in) THEN
+            
+				IF (counter > 100_000_000) THEN
+					IF (btn_add = '1') THEN
+						 counter := 0;
+					    queue(queue_pos) <= '1';
+						 queue_pos := queue_pos + 1;
+				   END IF;
+				   IF (btn_rm = '1') THEN
+						 counter := 0;
+					    queue(queue_pos) <= '0';
+						 queue_pos := queue_pos + 1;
+				   END IF;
+				END IF;
+				counter := counter + 1;
+            CASE state IS
+                    WHEN INIT =>
+                        state <= SHOW;
+                    WHEN SHOW =>
+						    IF (queue_pos > 0) THEN
+                        IF (queue(0) = '1') THEN
+									 queue_pos := queue_pos - 1;
+									 queue <= STD_LOGIC_VECTOR(SHIFT_LEFT(UNSIGNED(queue), 1));
+									 aux_level := (aux_level + 1);
+									 up_down := 1;
+                            state <= ANI;
+							   ELSIF (queue(0) = '0') THEN
+									 queue_pos := queue_pos - 1;
+									 queue <= STD_LOGIC_VECTOR(SHIFT_LEFT(UNSIGNED(queue), 1));
+									 aux_level := (aux_level - 1);
+									 up_down := 0;
+                            state <= ANI;
+                        END IF;
+							 END IF;
+						  WHEN ANI =>
+								  counter_level := counter_level + 1;
+						       IF (counter_level > 10_000_000) THEN
+										counter_level :=0;
+									IF (up_down_pos < 9 - aux_level) THEN
+										 up_down_pos := (up_down_pos + 1);
+										 IF(up_down = 1) THEN
+											 vector_level(9 - up_down_pos):= '1';
+											 vector_level(9 - up_down_pos - 1):= '0';
+										 ELSIF (up_down = 0) THEN
+											 vector_level(aux_level - up_down_pos):='1';
+											 vector_level(aux_level - up_down_pos + 1):= '0';
+										 END IF;
+									ELSIF (up_down_pos >= 9 - aux_level) THEN
+											 up_down_pos := 0;
+											 state <= SHOW;
+									END IF;
+								 END IF;
+                END CASE;
+				water_level <= aux_level; 
+				vector_out <= vector_level;
+				show_count <= queue_pos;
         END IF;
-
-    END PROCESS QUEUEING;
-    --process to execute the queues values
-    PROCESSING : PROCESS (clk, rst) IS
-        VARIABLE deposit : INTEGER;
-
-    BEGIN
-        IF (rst = '1') THEN
-            deposit := 0;
-        elsIF (rising_edge(clk)) THEN
-            x <= 0;
-            --only proceeds when queue_lenght bigger than 1
-            IF (queue_lenght_var >= 1) THEN
-                --adds a drop in the bucket
-                IF (queue(0) = '1') THEN
-                    --						output_generate_add : FOR i IN 0 TO n-1 LOOP
-                    --							IF (output_buffer(i) = '0') THEN
-                    --								output_buffer(i) <= '1';
-                    --								output <= output_buffer;
-                    --								--delay here?
-                    --								output_buffer(i) <= '0';
-                    --								output <= output_buffer;
-                    --							END IF;
-                    --						END LOOP output_generate_add;
-                    --						output_buffer(n-1-deposit) <= '1';
-                    --						output <= output_buffer;
-                    deposit := deposit + 1;
-                END IF;
-
-                --takes out a drop out of the bucket
-                IF (queue(0) = '0') THEN
-                    --						output_buffer(n-1-deposit) <= '0';
-                    --						output <= output_buffer;
-                    --						output_generate_remove : FOR i IN n-1 TO 0 LOOP
-                    --							IF (output_buffer(i) = '0') THEN
-                    --								output_buffer(i) <= '1';
-                    --								output <= output_buffer;
-                    --								--delay here?
-                    --								output_buffer(i) <= '0';
-                    --								output <= output_buffer;
-                    --							END IF;
-                    --						END LOOP output_generate_remove;
-                    deposit := deposit - 1;
-                END IF;
-
-            END IF;
-            output_buffer <= std_LOGIC_VECTOR(to_unsigned(deposit, 10));
-				deposit_sig <= deposit;
-            x             <= 1;
-        END IF;
-
-
-    END PROCESS PROCESSING;
+    END PROCESS;
 	 
-	     saidabin <= std_LOGIC_VECTOR(to_unsigned(deposit_sig, 12));
-
+	     -- code bellow to display on 7 segment display
+    final <=  0 WHEN state = INIT ELSE
+				  1 WHEN state = SHOW ELSE
+				  2 WHEN state = ANI ELSE
+				  3;
+    saidabin <= std_LOGIC_VECTOR(to_unsigned(final, 12));
+	 saidabin_count <= std_LOGIC_VECTOR(to_unsigned(water_level, 12));
+	 saidabin_water <= std_LOGIC_VECTOR(to_unsigned(water_level, 12));
     saida_GENERATE_FOR : FOR i IN 0 TO 3 GENERATE
         saidadecbin(i) <= saidabin(i);
     END GENERATE saida_GENERATE_FOR;
-
-    water_level <= "0000001" WHEN saidadecbin = "0000" ELSE
-        "1001111" WHEN saidadecbin = "0001" ELSE
-        "0010010" WHEN saidadecbin = "0010" ELSE
-        "0000110" WHEN saidadecbin = "0011" ELSE
-        "1001100" WHEN saidadecbin = "0100" ELSE
-        "0100100" WHEN saidadecbin = "0101" ELSE
-        "0100000" WHEN saidadecbin = "0110" ELSE
-        "0001111" WHEN saidadecbin = "0111" ELSE
-        "0000000" WHEN saidadecbin = "1000" ELSE
-        "0001100" WHEN saidadecbin = "1001" ELSE
-        "0001000" WHEN saidadecbin = "1010" ELSE
-        "1100000" WHEN saidadecbin = "1011" ELSE
-        "0110001" WHEN saidadecbin = "1100" ELSE
-        "1000010" WHEN saidadecbin = "1101" ELSE
-        "0110000" WHEN saidadecbin = "1110" ELSE
-        "0111000" WHEN saidadecbin = "1111" ELSE
-        "1111111";
 	 
+	 out_GENERATE_FOR : FOR i IN 0 TO 3 GENERATE
+        saidadecbin_count(i) <= saidabin_count(i);
+    END GENERATE out_GENERATE_FOR;
+	 
+	 water_GENERATE_FOR : FOR i IN 0 TO 9 GENERATE
+        saidadecbin_water(i) <= saidabin_water(i);
+    END GENERATE water_GENERATE_FOR;
+	 
+	 output_led <= saidadecbin_water;
+	 
+						  
+output_seg <= "0000001" WHEN saidadecbin_count = "0000" ELSE
+						  "1001111" WHEN saidadecbin_count = "0001" ELSE
+						  "0010010" WHEN saidadecbin_count = "0010" ELSE
+						  "0000110" WHEN saidadecbin_count = "0011" ELSE
+						  "1001100" WHEN saidadecbin_count = "0100" ELSE
+						  "0100100" WHEN saidadecbin_count = "0101" ELSE
+						  "0100000" WHEN saidadecbin_count = "0110" ELSE
+						  "0001111" WHEN saidadecbin_count = "0111" ELSE
+						  "0000000" WHEN saidadecbin_count = "1000" ELSE
+						  "0001100" WHEN saidadecbin_count = "1001" ELSE
+						  "0001000" WHEN saidadecbin_count = "1010" ELSE
+						  "1100000" WHEN saidadecbin_count = "1011" ELSE
+						  "0110001" WHEN saidadecbin_count = "1100" ELSE
+						  "1000010" WHEN saidadecbin_count = "1101" ELSE
+						  "0110000" WHEN saidadecbin_count = "1110" ELSE
+						  "0111000" WHEN saidadecbin_count = "1111" ELSE
+						  "1111111";
+	 
+	 
+    output_seg_count <="0000001" WHEN saidadecbin = "0000" ELSE
+					  "1001111" WHEN saidadecbin = "0001" ELSE
+					  "0010010" WHEN saidadecbin = "0010" ELSE
+					  "0000110" WHEN saidadecbin = "0011" ELSE
+					  "1001100" WHEN saidadecbin = "0100" ELSE
+					  "0100100" WHEN saidadecbin = "0101" ELSE
+					  "0100000" WHEN saidadecbin = "0110" ELSE
+					  "0001111" WHEN saidadecbin = "0111" ELSE
+					  "0000000" WHEN saidadecbin = "1000" ELSE
+					  "0001100" WHEN saidadecbin = "1001" ELSE
+					  "0001000" WHEN saidadecbin = "1010" ELSE
+					  "1100000" WHEN saidadecbin = "1011" ELSE
+					  "0110001" WHEN saidadecbin = "1100" ELSE
+					  "1000010" WHEN saidadecbin = "1101" ELSE
+					  "0110000" WHEN saidadecbin = "1110" ELSE
+					  "0111000" WHEN saidadecbin = "1111" ELSE
+					  "1111111";
+	
 END ARCHITECTURE;
